37c9c0c64ee99c355cb5758d8b288093ea015e94
vindimm <vindimm@bk.ru>
2023-02-17 21:40:58 +0500

    добавил фавиконку и минификацию css

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ import htmlmin from 'gulp-htmlmin';
 import sass from 'gulp-dart-sass';
 import plumber from 'gulp-plumber';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
 import del from 'del';
 
 // HTML
@@ -21,7 +22,8 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
@@ -70,6 +72,17 @@ const watcher = () => {
   gulp.watch('source/scss/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html', gulp.series(html, reload));
 }
+
+// Build
+export const build = gulp.series(
+  buildClean,
+  copy,
+  imagesCopy,
+  gulp.parallel(
+    styles,
+    html,
+  )
+);
 
 export default gulp.series(
   buildClean,
